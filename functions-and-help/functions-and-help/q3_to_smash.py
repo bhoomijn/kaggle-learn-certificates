4e8dcee443578fa884d2f354994e7e30a1c123dd
Bhoomi Jain <bhoomijn4@gmail.com>
2026-07-03 18:03:22 +0530

    Create q3_to_smash.py

---
+++ functions-and-help/functions-and-help/q3_to_smash.py
@@ -0,0 +1,30 @@
+# Functions and Getting Help - Q3
+# Modify the candy-sharing function to accept optional number of friends
+
+def to_smash(total_candies, n_friends=3):
+    """Return the number of leftover candies that must be smashed after distributing
+    the given number of candies evenly between friends.
+
+    Parameters:
+    total_candies (int): Total number of candies collected.
+    n_friends (int, optional): Number of friends to split candies between.
+                               Defaults to 3.
+
+    Examples:
+    >>> to_smash(91)
+    1
+    >>> to_smash(91, 7)
+    0
+    >>> to_smash(10, 2)
+    0
+    """
+    return total_candies % n_friends
+
+
+# Example usage
+print("Default (3 friends):", to_smash(91))       # Expected output: 1
+print("Custom (7 friends):", to_smash(91, 7))    # Expected output: 0
+print("Two friends:", to_smash(10, 2))           # Expected output: 0
+
+# Kaggle grader check (keep only in Kaggle notebook)
+q3.check()
